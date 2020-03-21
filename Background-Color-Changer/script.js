cfd4862b833d6be7639163d5c7667fdc70e7bf2a
Manish Virgat <54372006+manish-virgat@users.noreply.github.com>
2020-03-22 00:09:05 +0530

    Add files via upload

---
+++ Background-Color-Changer/script.js
@@ -0,0 +1,12 @@
+const colorbtn = document.querySelector('.colorbtn');
+const bodyBcg = document.querySelector('body');
+
+const colors = ['yellow','red','green','#3b5998','white','purle','pink'];
+
+colorbtn.addEventListener('click',changeColor);
+function changeColor()
+{
+    //bodyBcg.style.backgroundColor = colors[2];
+    let random = Math.floor(Math.random()*colors.length);
+    bodyBcg.style.backgroundColor = colors[random];
+}
